7d79743bedc6ad8c4e9454bb11261a5937a40b9a
Keith Wickramasekara <keith.wi@live.com>
2021-04-05 19:56:22 -0500

    Build JS with esbuild

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
-const sass = require("sass");
 const fs = require("fs");
+const sass = require("sass");
+const esbuild = require("esbuild");
 
 // Default directories and filenames. Change if necessary.
 const SOURCE_DIR = "./_src";
@@ -8,6 +9,8 @@ const SCSS_DIR = "./_src/assets/scss";
 const SCSS_FILE = "main.scss";
 const CSS_OUTPUT_DIR = "./_build/assets/css";
 const CSS_FILE = "main.css";
+const JS_DIR = "./_src/assets/js";
+const JS_FILE = "main.js";
 
 let firstBuildRan = false;
 
@@ -29,6 +32,27 @@ compileSass = () => {
   }
 };
 
+compileJS = () => {
+  try {
+    let startTime = Date.now();
+
+    let build = esbuild.buildSync({
+      entryPoints: ["./_src/assets/js/main.js"],
+      bundle: true,
+      minify: true,
+      outfile: "./_build/assets/js/main.js",
+    });
+
+    let endTime = Date.now();
+
+    console.log(`JS compiled in ${((endTime - startTime) / 1000).toFixed(2)} seconds`);
+
+    if (build.warnings.length > 0) console.log(JSON.stringify(build.warnings));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 module.exports = (eleventyConfig) => {
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
@@ -40,16 +64,20 @@ module.exports = (eleventyConfig) => {
     .on("beforeBuild", () => {
       if (!firstBuildRan) {
         compileSass();
+        compileJS();
         firstBuildRan = true;
       }
     })
     .on("beforeWatch", (changedFiles) => {
       if (changedFiles.includes(`${SCSS_DIR}/${SCSS_FILE}`)) {
         compileSass();
+      } else if (changedFiles.includes(`${JS_DIR}/${JS_FILE}`)) {
+        compileJS();
       }
     });
 
   eleventyConfig.addWatchTarget(`${SCSS_DIR}/`);
+  eleventyConfig.addWatchTarget(`${JS_DIR}/`);
 
   return {
     // Control which files Eleventy will process
